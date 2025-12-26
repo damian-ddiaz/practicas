73b7c80fcea31eb5dfe162c2825305a357979636
D & D sistemas <damiandiaz841@gmail.com>
2025-12-26 14:39:56 -0400

    Actualizacion 26-12-2025

--- PHP/conciliaciones/menu/menu_inventario_actuliza_menu.php
+++ PHP/conciliaciones/menu/menu_inventario_actuliza_menu.php
@@ -10,91 +10,91 @@ buscardatoslogin([usr_login]);
 // Administracion Inicio
 // Adminidtrativo - Contabilidad - Finanzas - Recursos Humanos
 	if([permite_url_portal_administrativo] === 'NO'){
-		sc_menu_disable(item_45);
+		sc_menu_disable(item_718);
 	}
 	if([permite_url_portal_contabilidad] === 'NO'){
-		sc_menu_disable(item_46);
+		sc_menu_disable(item_719);
 	}
 	if([permite_url_portal_finanzas] === 'NO'){
-		sc_menu_disable(item_47);
+		sc_menu_disable(item_720;
 	}
 	if([permite_url_portal_rrhh] === 'NO'){
-		sc_menu_disable(item_48);
+		sc_menu_disable(item_721);
 	}
 // Administracion Fin
 
 // Operaciones Inicio
 // Dispositivos - Inventario - Mantenimiento - Pos - Tareas y Proyectos
 	if([permite_url_portal_dispositos] === 'NO'){
-		sc_menu_disable(item_51);
+		sc_menu_disable(item_722);
 	}
 	if([permite_url_portal_inventario] === 'NO'){
-		sc_menu_disable(item_52);
+		sc_menu_disable(item_723);
 	}
 	if([permite_url_portal_mantenimiento] === 'NO'){
-		sc_menu_disable(item_53);
+		sc_menu_disable(item_724);
 	}
 	if([permite_url_portal_puntodeventa] === 'NO'){
-		sc_menu_disable(item_54);
+		sc_menu_disable(item_725;
 	}
 	if([permite_url_portal_proyectos] === 'NO'){
-		sc_menu_disable(item_50);
+		sc_menu_disable(item_726);
 	}
 // Operaciones Fin
 
 // Comercial Inicio
 // Aliados - Atencion Comercial- Eventos - Patrocinantes - Vendedor
 	if([permite_url_portal_aliado] === 'NO'){
-		sc_menu_disable(item_56);
+		sc_menu_disable(item_727);
 	}
 	if([permite_url_portal_atencion_comercial] === 'NO'){
-		sc_menu_disable(item_57);
+		sc_menu_disable(item_728);
 	}
 	if([permite_url_portal_eventos] === 'NO'){
-		sc_menu_disable(item_58);
+		sc_menu_disable(item_729);
 	}
 	if([permite_url_portal_patrocinadores] === 'NO'){
-		sc_menu_disable(item_59);
+		sc_menu_disable(item_730;
 	}
 
 	if([permite_url_portal_vendedor] === 'NO'){
-		sc_menu_disable(item_60);
+		sc_menu_disable(item_731;
 	}
 // Comercial Fin
 
 // Soporte y Herramientas - Inicio
 // Gestion de Redes - Helpdesk - Herramientas - Soporte Tecnico
 	if([permite_url_portal_gestionredes] === 'NO'){
-		sc_menu_disable(item_62);
+		sc_menu_disable(item_732);
 	}
 	if([permite_url_portal_helpdesk] === 'NO'){
-		sc_menu_disable(item_63);
+		sc_menu_disable(item_733);
 	}
 	if([permite_url_portal_herramientas] === 'NO'){
-		sc_menu_disable(item_64);
+		sc_menu_disable(item_734);
 	}
 	if([permite_url_portal_soporte] === 'NO'){
-		sc_menu_disable(item_65);
+		sc_menu_disable(item_735);
 	}
 // Soporte y Herramientas Fin
 
 // Reportes - Inicio
 // Reportes Empresa - Reportes Sucursal
 	if([permite_url_portal_reportes_matriz] === 'NO'){
-		sc_menu_disable(item_67);
+		sc_menu_disable(item_736);
 	}
 	if([permite_url_portal_reportes] === 'NO'){
-		sc_menu_disable(item_68);
+		sc_menu_disable(item_737);
 	}
 // Reportes Fin
 
 // Otros - Inicio
 // Configucaion - Intranet
 	if([permite_url_portal_configuracion] === 'NO'){
-		sc_menu_disable(item_70);
+		sc_menu_disable(item_738);
 	}
 	if([permite_url_portal_intranet] === 'NO'){
-		sc_menu_disable(item_71);
+		sc_menu_disable(item_739);
 	}
 // Otros Fin
 
